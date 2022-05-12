32b61bd6df2a040b62ac038d38ade06ab4429e02
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-12 20:16:26 +0300

    feat: add basic project structure

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+import isWebp from './modules/webp.js'
+import burgerMenu from './modules/burger-menu.js'
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  isWebp();
+  burgerMenu();
+});
